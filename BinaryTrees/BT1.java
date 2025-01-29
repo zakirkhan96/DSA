5dc609c77414b6fbd6c106852803ce2d5ed6a13e
zakirkhan96 <zk548473@gmail.com>
2025-01-29 18:28:01 +0500

    Implementing Binary trees

--- BinaryTrees/BT1.java
+++ BinaryTrees/BT1.java
@@ -2,6 +2,7 @@ package BinaryTrees;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Scanner;
 
 public class BT1 {
     public static class Node {
@@ -102,7 +103,117 @@ public class BT1 {
                 }
             }
         }
+
+        public void SumOfLevel(Node root , int k){
+            int level = 0;
+            int  sum =  0;
+            Queue<Node> q2 = new LinkedList<>();
+
+            q2.add(root);
+            q2.add(null);
+
+            while (!q2.isEmpty()) {
+                Node currNode = q2.remove();
+
+                if (currNode == null) {
+
+                    level++;
+
+                    if (q2.isEmpty()) {
+
+                        break;
+
+                    }else{
+
+                        q2.add(null);
+
+                    }
+                }else{
+
+                    if (level == k) {
+                        sum += currNode.data;
+                    }
+
+                    if (currNode.left != null) {
+
+                        q2.add(currNode.left);
+
+                    }
+
+                    if (currNode.right != null) {
+                    
+                        q2.add(currNode.right);
+                    
+                    }
+                }
+            }
+
+            System.out.println("The sum of Nodes at level " + level + " is : " + sum);
+        }
     
+        public int CountOfNode(Node root){
+            if (root == null) {
+                return 0;
+            }
+
+            int leftNodes = CountOfNode(root.left);
+            int rightNodes = CountOfNode(root.right);
+
+            return leftNodes + rightNodes + 1;
+        }
+
+        public int SumOfNodes(Node root){
+            if (root == null) {
+                return 0;
+            }
+
+            int leftSumOfNodes = SumOfNodes(root.left);
+            int rightSumOfNodes = SumOfNodes(root.right);
+
+            return leftSumOfNodes + rightSumOfNodes + root.data;
+        }
+
+        public int heightOfTree(Node root){
+            if (root == null) {
+                return 0;
+            }
+
+            int leftHeight = heightOfTree(root.left);
+            int rightHeight = heightOfTree(root.right);
+
+            int myHeight = Math.max(leftHeight, rightHeight) + 1;
+
+            return myHeight;
+        }
+
+        public class TreeInfo {
+            int ht , diam;
+
+            TreeInfo(int ht , int diam){
+                this.diam = diam;
+                this.ht = ht;
+            }
+        }
+
+        public TreeInfo diameter(Node root){
+            if (root == null) {
+                return new TreeInfo(0, 0);
+            }
+
+            TreeInfo leftDiam = diameter(root.left);
+            TreeInfo rightDiam = diameter(root.right);
+
+            int myHeight2 = Math.max(leftDiam.ht, rightDiam.ht) + 1;
+
+            int diam = leftDiam.diam;
+            int diam2 = rightDiam.diam;
+            int diam3 = leftDiam.ht + rightDiam.ht + 1;
+
+            int myDiam = Math.max(Math.max(diam, diam2), diam3);
+            TreeInfo myInfo = new TreeInfo(myHeight2, myDiam);
+
+            return myInfo;
+        }
         
         public static void main(String[] args) {
             int nodes[] = {1 , 2 , 4 , -1 , -1 , 5 , -1 , -1 , 3 , -1 , 6 , -1 , -1};
@@ -120,6 +231,26 @@ public class BT1 {
             tree.preOrder(root);
             System.out.println("Level-Order traversal: ");
             tree.levelOrder(root);
+
+            // Now on its operation on Binary tree
+            System.out.println("Operations of Tree:");
+            System.out.println("Count of Nodes in a tree: ");
+            System.out.println(tree.CountOfNode(root));
+
+            System.out.println("Sum Of Nodes in tree: ");
+            System.out.println(tree.SumOfNodes(root));
+
+            System.out.println("Height of tree: ");
+            System.out.println(tree.heightOfTree(root));
+
+            System.out.println("Diameter of Tree: ");
+            System.out.println(tree.diameter(root).diam);
+
+            System.out.println("Enter the level to calculate sum fo Nodes at that level: ");
+            Scanner scanner = new Scanner(System.in);
+            int k = scanner.nextInt();
+            tree.SumOfLevel(root, k);
+            scanner.close();
 
         }
     }
